4117e811a63a76d94f0a3f651439fdc45c7d5b0e
sachin98765 <119880539+sachin98765@users.noreply.github.com>
2025-01-18 16:10:54 +0530

    Update login-check.php

--- admin/partials/login-check.php
+++ admin/partials/login-check.php
@@ -9,8 +9,8 @@ if(!isset($_SESSION['user']))            ///if user session is not set
 
 
     //redirect login page with meassage
-    $_SESSION['no-login-message'] =" <div class='error text-center'>Please login to access Admin Panal</div>";
-    header('location:'.SITEURL.'../admin/login.php');
+    $_SESSION['no-login-message'] =" <div class='error text-center'>Please login to access Admin Panalllll</div>";
+    header('location:'.SITEURL.'../admin/partials/login.php');
 }
 
 ?>
